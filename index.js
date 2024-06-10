b91bf346aa1a8f42534693750a7c9178f34890c0
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-06-10 10:47:30 -0400

    get

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const express = require('express')
     app.get('/', (req, res) => {
         res.send('Hello World');
     })
+
+    app.get('/about', (req, res) => {
+        res.send('<h1>About page</h1>')
+    })
+    
